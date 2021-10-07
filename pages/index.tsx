3a3b72bcd2790839fa7fb07be84a95d501f4eee9
Yamagishi Kazutoshi <ykzts@desire.sh>
2021-10-08 00:31:53 +0900

    Add Hello component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Hello from '@/components/hello'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,6 +15,8 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          <Hello />
+          {' '}
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
